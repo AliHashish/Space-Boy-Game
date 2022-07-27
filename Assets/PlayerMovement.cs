7736b9e74d055eb3417423838eac55afddd9fce4
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-07-27 14:57:53 +0200

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Rigidbody2D body;
+
+    // Some controls
+    public float runSpeed = 40f;    
+    float horizontalMove = 0f;      // movement on x-axis
+    bool jump = false;
+    bool crouch = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));  // Prints in terminal
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; // A: -1 , D: 1
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        else if(Input.GetButtonUp("Jump"))
+        {
+            
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    // Gets called every fixed amount of time
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        // Move(el sor3a, crouch, jump)
+        // Time.fixedDeltaTime btedman eno yt7rk b sor3a consistent
+        jump = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# UnityTask1
+ 
