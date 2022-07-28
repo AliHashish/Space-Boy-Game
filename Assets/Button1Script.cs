28d9a0c46d47b7564dffdb528deb964a6d4b75ab
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-07-28 03:34:27 +0200

    Button1 Done
    5lst animation el button

--- Assets/Button1Script.cs
+++ Assets/Button1Script.cs
@@ -11,8 +11,14 @@ public class Button1Script : MonoBehaviour
     // 3rft object b ay esm 3ady
     // b3dein haroo7 fy unity a-attach el object dh bl ana 3ayzo
     public GameObject Block;
+    
+    public Sprite pressedButton;
 
     bool trigger = false;
+    bool pressed = false;
+
+    private Sprite oldSprite;
+    int x = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +31,7 @@ public class Button1Script : MonoBehaviour
         // {
         //     Block.SetActive(true);
         // }
+        oldSprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
     }
 
     // Update is called once per frame
@@ -35,6 +42,7 @@ public class Button1Script : MonoBehaviour
             customEvent.Invoke();    //hy-invoke kol el events
             trigger = false;
         }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -47,8 +55,29 @@ public class Button1Script : MonoBehaviour
         // Tag dyh htla2eeha fy awl satr kda ganb el layer fl player
         if (other.gameObject.CompareTag("Player"))
         {
-            trigger = true;
-            // Debug.Log("Da5al el if condition kaman ");  // Prints in terminal
+            x++;
+            if(x%2==0)
+            {
+                x=0;    // b7eith adman en el x mt3mlsh overflow xD
+                        // kan momkn ast3ml bool a7sn
+            }
+            else
+            {
+                trigger = true;
+                pressed = !pressed;
+                if(pressed)
+                {
+                    Debug.Log("Pressed");
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = pressedButton;
+                }
+                else
+                {
+                    Debug.Log("NOT Pressed");
+                    this.gameObject.GetComponent<SpriteRenderer>().sprite = oldSprite;
+                }
+                // Debug.Log(x);
+                // Debug.Log("Da5al el if condition kaman ");  // Prints in terminal
+            }
         }
     }
 }
